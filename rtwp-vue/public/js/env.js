c4f464b9d13229fcb803ab3ab6f84c1844f395b1
Mike Echevarria <mike.echevarria@gmail.com>
2020-09-23 15:48:33 -0400

    generate env.js only

--- rtwp-vue/public/js/env.js
+++
@@ -1,8 +0,0 @@
-// replaced as env.js at runtime by envsubst
-(function (window) {
-  window._env = window._env || { keycloak: {} };
-  window._env.keycloak.url = 'https://rtwp-keycloak.cfapps.us10.hana.ondemand.com/auth' || 'http://localhost:8080/auth';
-  window._env.keycloak.realm = '' || 'cp-sso';
-  window._env.keycloak.id = '' || 'vue';
-  window._env.keycloak.enabled = 'false' || 'false';
-})(this);
