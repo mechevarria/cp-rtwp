0ceece97a52a55167ba4aa701c73bd90c253afd3
Mike Echevarria <mike.echevarria@gmail.com>
2020-09-23 17:34:44 -0400

    cloud platform keycloak integration

--- rtwp-vue/cf-custom-command.sh
+++ rtwp-vue/cf-custom-command.sh
@@ -11,5 +11,9 @@ search='${KEYCLOAK}'
 replace=$KEYCLOAK
 sed -i s@$search@$replace@g ./dist/js/env.js
 
+search='http://localhost:4200'
+replace=$EXPRESS_URL
+sed -i s@$search@$replace@g ./nginx.conf
+
 echo "Running buildpack command"
 varify -buildpack-yml-path ./buildpack.yml ./nginx.conf $HOME/modules $DEP_DIR/nginx/modules && nginx -p $PWD -c ./nginx.conf
--- rtwp-vue/public/js/env.template.js
+++ rtwp-vue/public/js/env.template.js
@@ -2,7 +2,7 @@
 (function (window) {
   window._env = window._env || { keycloak: {} };
   window._env.keycloak.url = '${KEYCLOAK_URL}' || 'http://localhost:8080/auth';
-  window._env.keycloak.realm = '${KEYCLOAK_REALM}' || 'rtwp';
-  window._env.keycloak.id = '${KEYCLOAK_ID}' || 'vue';
+  window._env.keycloak.realm = 'rtwp';
+  window._env.keycloak.id = 'vue';
   window._env.keycloak.enabled = '${KEYCLOAK}' || 'false';
 })(this);
