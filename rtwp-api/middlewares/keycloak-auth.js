0adcff4b5f9be8ef022a2c7e3d0cc5e76c80e31d
Mike Echevarria <mike.echevarria@gmail.com>
2020-09-23 14:16:53 -0400

    refactor to rtwp

---
+++ rtwp-api/middlewares/keycloak-auth.js
@@ -0,0 +1,37 @@
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
+
+module.exports = ((app) => {
+    let middleware = {};
+
+    if (process.env.KEYCLOAK === 'true') {
+        console.info('Securing with keycloak');
+
+        const kcConfig = {
+            resource: 'express',
+            'bearer-only': true,
+            'auth-server-url': process.env.KEYCLOAK_URL || 'http://localhost:8180/auth',
+            realm: 'rtwp'
+        };
+        const memoryStore = new session.MemoryStore();
+        const keycloak = new Keycloak({ store: memoryStore }, kcConfig);
+
+        app.use(session({
+            secret: 'keycloak-session-secret',
+            resave: false,
+            saveUninitialized: true,
+            store: memoryStore
+        }));
+
+        app.use(keycloak.middleware());
+
+        middleware = keycloak.protect();
+    } else {
+        console.info('Keycloak is disabled');
+        middleware = (req, res, next) => {
+            next();
+        };
+    }
+
+    return middleware;
+});
---
+++ rtwp-api/server.js
@@ -0,0 +1,34 @@
+const compression = require('compression');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const xsenv = require('@sap/xsenv');
+const hdbext = require('@sap/hdbext');
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(compression());
+
+// add HANA client to all incoming requests. json file is only read when not running on XS Advanced Server
+const services = xsenv.getServices({ hana: { tag: 'hana' } }, '/tmp/default-services.json');
+app.use('/', hdbext.middleware(services.hana));
+
+// configure keycloak if enabled
+const keycloakAuth = require('./middlewares/keycloak-auth')(app);
+
+app.use(keycloakAuth);
+
+const router = express.Router();
+const statusCtrl = require('./controllers/status');
+
+router.route('/status').get(statusCtrl);
+
+app.use('/', router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.info(`http server started on port ${port}`);
+});
