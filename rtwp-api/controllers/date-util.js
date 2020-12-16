ed1797f06846b964451c7e1da3c22b9c5aa15f96
Mike Echevarria <mike.echevarria@gmail.com>
2020-12-16 12:13:08 -0500

    minor fix formatting

--- rtwp-api/controllers/date-util.js
+++ rtwp-api/controllers/date-util.js
@@ -1,17 +1,17 @@
 const moment = require('moment');
 
 function getDefaults() {
-    const now = moment();
-    const threeMonthsBack = now.clone().subtract(90, 'days');
+  const now = moment();
+  const threeMonthsBack = now.clone().subtract(90, 'days');
 
-    const defaults = {
-        start: threeMonthsBack.format('YYYY-MM-DD'),
-        end: now.format('YYYY-MM-DD')
-    };
+  const defaults = {
+    start: threeMonthsBack.format('YYYY-MM-DD'),
+    end: now.format('YYYY-MM-DD')
+  };
 
-    return defaults;
+  return defaults;
 }
 
 module.exports = {
-    getDefaults
+  getDefaults
 };
--- rtwp-api/controllers/status.js
+++ rtwp-api/controllers/status.js
@@ -1,8 +1,8 @@
 module.exports = (req, res) => {
-    const name = req.query.name || 'Guest';
+  const name = req.query.name || 'Guest';
 
-    res.json({
-        message: `Greetings ${name} from the nodes.js Express server`,
-        time: new Date()
-    });
+  res.json({
+    message: `Greetings ${name} from the nodes.js Express server`,
+    time: new Date()
+  });
 };
