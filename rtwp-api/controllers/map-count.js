d222dcdccb2a9094ebd1acfb0d79a3689b57ee9e
Mike Echevarria <mike.echevarria@gmail.com>
2020-12-16 12:12:32 -0500

    refactor to map from generic hana

---
+++ rtwp-api/controllers/map-count.js
@@ -0,0 +1,29 @@
+const dateUtil = require('./date-util');
+
+module.exports = (req, res) => {
+  const defaults = dateUtil.getDefaults();
+
+  const start = req.query.start || defaults.start;
+  const end = req.query.end || defaults.end;
+
+  const sql = `
+    SELECT 
+        count(*) AS "count",
+        MIN(SEEN_TS) AS "min",
+        MAX(SEEN_TS) AS "max"
+    FROM badge_location
+    WHERE SEEN_TS >= TO_TIMESTAMP( ?, 'YYYY-MM-DD' ) AND SEEN_TS <= TO_TIMESTAMP( ?, 'YYYY-MM-DD' )`;
+
+  try {
+    const result = req.db.exec(sql, [start, end])[0];
+
+    res.status(200).json({
+      result
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: `[SQL Execute error]: ${err.message}`
+    });
+  }
+};
---
+++ rtwp-api/controllers/map.js
@@ -0,0 +1,40 @@
+const dateUtil = require('./date-util');
+
+module.exports = (req, res) => {
+  const defaults = dateUtil.getDefaults();
+
+  const start = req.query.start || defaults.start;
+  const end = req.query.end || defaults.end;
+
+  const sql = `
+    SELECT 
+        loc as "loc",
+        count(*) AS "locCount",
+        geo_loc.ST_AsGeoJSON() as "geoLoc"
+    FROM badge_location
+    WHERE
+        SEEN_TS >= TO_TIMESTAMP(?, 'YYYY-MM-DD') and SEEN_TS <= TO_TIMESTAMP(?, 'YYYY-MM-DD')
+    GROUP BY 
+        loc,
+        geo_loc
+    ORDER BY count(*) DESC
+    LIMIT 50`;
+
+  try {
+    const results = req.db.exec(sql, [start, end]);
+
+    results.forEach((result) => {
+      // eslint-disable-next-line no-param-reassign
+      result.geoLoc = JSON.parse(result.geoLoc);
+    });
+
+    res.status(200).json({
+      results
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: `[SQL Execute error]: ${err.message}`
+    });
+  }
+};
