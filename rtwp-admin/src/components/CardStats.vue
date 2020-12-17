a0c14dac35279304a6ad488715c95ee82bcabef6
Mike Echevarria <mike.echevarria@gmail.com>
2020-12-17 14:22:30 -0500

    replace placeholder chart with basic visitor stats

--- rtwp-admin/src/components/CardStats.vue
+++ rtwp-admin/src/components/CardStats.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="card">
-    <div class="card-header">Statistics <i class="float-right cil-chart-pie"></i></div>
+    <div class="card-header">
+      <i class="spinner-border spinner-border-sm mr-1" v-if="isBusy"></i> Statistics
+      <i class="float-right cil-chart-pie"></i>
+    </div>
     <div class="card-body">
       <ul class="list-inline">
         <li>
@@ -12,6 +15,7 @@
 </template>
 <script>
 import { Chart } from 'highcharts-vue'
+import axios from 'axios'
 
 export default {
   name: 'AppCardStats',
@@ -20,32 +24,48 @@ export default {
   },
   data() {
     return {
+      isBusy: false,
       chartOptions: {
         chart: {
           type: 'pie',
-          height: '200px'
+          height: '250px'
         },
         title: {
-          text: 'Visitor Metrics'
+          text: 'Visitors By Month'
         },
         series: [
           {
-            data: [
-              {
-                y: 60,
-                name: 'Scheduled',
-                color: '#321fdb'
-              },
-              {
-                y: 40,
-                name: 'Ad-hoc',
-                color: '#3399ff'
-              }
-            ]
+            data: []
           }
         ]
       }
     }
+  },
+  methods: {
+    getData() {
+      this.isBusy = true
+      const url = '/rtwp-api/stats'
+      axios
+        .get(url)
+        .then(res => {
+          this.chartOptions.series[0].data = res.data.results.map(result => {
+            if (result.name == null) {
+              result.name = 'Unknown'
+            }
+            return result
+          })
+        })
+        .catch(err => {
+          console.error(err)
+          this.errorMsg(err.message)
+        })
+        .finally(() => {
+          this.isBusy = false
+        })
+    }
+  },
+  created() {
+    this.getData()
   }
 }
 </script>
