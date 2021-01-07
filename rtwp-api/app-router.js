17e1c2050a86a72e922ef7d7ae8eb390cc4b8706
Mike Echevarria <mike.echevarria@gmail.com>
2021-01-07 16:58:38 -0500

    separate routes now since we have a bunch

---
+++ rtwp-api/app-router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const map = require('./controllers/map');
+const mapCount = require('./controllers/map-count');
+const visitorAll = require('./controllers/visitor-all');
+const visitor = require('./controllers/visitor');
+const stats = require('./controllers/stats-visit');
+const visitorMap = require('./controllers/visitor-map');
+const visitorLength = require('./controllers/visitor-length');
+const dateDiff = require('./controllers/date-diff');
+const dateLast = require('./controllers/date-last');
+const dateStart = require('./controllers/date-start');
+
+const router = express.Router();
+
+router.route('/map').get(map);
+router.route('/map/count').get(mapCount);
+router.route('/visitors').get(visitorAll);
+router.route('/visitor').get(visitor);
+router.route('/stats').get(stats);
+router.route('/visitor/map').get(visitorMap);
+router.route('/visitor/length').get(visitorLength);
+router.route('/date/diff').get(dateDiff);
+router.route('/date/last').get(dateLast);
+router.route('/date/start').get(dateStart);
+
+module.exports = router;
