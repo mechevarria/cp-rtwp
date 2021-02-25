b0fbfd54b47d52103e7ddf4a50dd8dbd18ab81ec
Mike Echevarria <mike.echevarria@gmail.com>
2021-02-25 16:06:01 -0500

    add status controller back for testing

---
+++ rtwp-api/controllers/status.js
@@ -0,0 +1,8 @@
+module.exports = (req, res) => {
+  const name = req.query.name || 'Guest';
+
+  res.json({
+    message: `Greetings ${name} from the nodes.js Express server`,
+    time: new Date()
+  });
+};
